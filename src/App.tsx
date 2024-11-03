c9e8b74ba4e1d0b0640861aeeed028884f263272
James Koh <huisoonkoh@gmail.com>
2024-11-04 02:13:13 +0800

    Add side menu, and add playlist loading service

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import Player from "./player/Player.tsx";
+import YouTubePlayer from "./components/youtube-player/YouTubePlayer.tsx";
+import SideMenu from "./components/side-menu/SideMenu.tsx";
+import { useQuery } from "react-query";
+import { getPlaylists } from "./services/Playlists.service.ts";
 
 function App() {
   const playlistId = "PLUl4u3cNGP62EXoZ4B3_Ob7lRRwpGQxkb";
 
+  const {
+    data: playlistResults,
+    error,
+    isLoading,
+  } = useQuery("", async () => getPlaylists("invalid-token"));
+
+  if (error) {
+    return <>{`Failed loading playlists! Error: ${error}`}</>;
+  }
+
+  if (isLoading || !playlistResults) {
+    return <>Loading</>;
+  }
+
   return (
-    <div className="box-border h-screen">
-      <Player playlistId={playlistId} />
+    <div className="flex h-screen">
+      <SideMenu playlistResults={playlistResults} />
+      <YouTubePlayer playlistId={playlistId} />
     </div>
   );
 }
