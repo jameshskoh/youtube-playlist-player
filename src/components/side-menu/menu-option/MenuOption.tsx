b0bf65bf24632979055eb6fcc15c2d12c00657ee
James Koh <huisoonkoh@gmail.com>
2024-11-09 22:20:33 +0800

    Beautify playlist options

--- src/components/side-menu/menu-option/MenuOption.tsx
+++ src/components/side-menu/menu-option/MenuOption.tsx
@@ -6,16 +6,20 @@ export type MenuOptionProps = {
 
 const MenuOption = (props: MenuOptionProps) => {
   return (
-    <div
-      onClick={() => props.handleSelectPlaylist(props.playlistId)}
-      className="m-1 flex h-24 items-center rounded-sm bg-white p-4 shadow-md"
-    >
-      <div className="flex h-12 w-12 flex-shrink-0 items-center justify-center">
-        <div className="flex h-12 w-12 flex-shrink-0 items-center justify-center bg-amber-200"></div>
-      </div>
-      <div className="ml-4 flex flex-col">
-        <h3 className="text-lg font-bold text-gray-900">{props.title}</h3>
-      </div>
+    <div>
+      <button className="block w-full">
+        <div
+          onClick={() => props.handleSelectPlaylist(props.playlistId)}
+          className="mx-3 my-1 flex h-24 items-center rounded-lg bg-white p-4 shadow-md"
+        >
+          <div className="flex h-12 w-12 flex-shrink-0 items-center justify-center">
+            <div className="flex h-12 w-12 flex-shrink-0 items-center justify-center bg-amber-200"></div>
+          </div>
+          <div className="ml-4 flex flex-col">
+            <h3 className="text-lg font-bold text-gray-900">{props.title}</h3>
+          </div>
+        </div>
+      </button>
     </div>
   );
 };
